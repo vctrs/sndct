f745aaca873312892b0122d14feaf8e364d7e7d8
vctrs <43434588+vctrs@users.noreply.github.com>
2024-04-10 23:28:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.module.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.163.0/build/three.module.js";
 console.log('here');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
