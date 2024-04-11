aad615f90822103997896b43e2939346be3a4feb
vctrs <43434588+vctrs@users.noreply.github.com>
2024-04-11 13:06:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig} from 'vite'
 
 export default defineConfig({
  build: {
-    base: "./sndct",
+    base: "/sndct/",
     minify: false,
     target: 'esnext',
     rollupOptions: 
